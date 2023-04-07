4c0ab263402f215cae8ba2b2b3b8e5b23f034c67
jay7u3 <116310118+jay7u3@users.noreply.github.com>
2023-04-07 19:18:40 +0200

    update système surface, refatcorisation, gigachad

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ class Joueur:
                 return True
         return False
     
-    def actualiser_surface(self, liste_obstacles:list):
+    def actualiser_surface(self, liste_obstacle:list):
         """
         actualiser_surface() permet de gerer la hauteur de la surface afin de pouvoir sauter sur des
         éléments par exemple.
@@ -74,45 +74,53 @@ class Joueur:
         pass
 
 # Création d'une liste d'obstacle
+def niveau_obstacle()->List[Obstacle]:
+    """
+    Fonction de création des obstacles du niveau.
+    """
+    liste_obstacle = []
+    liste_obstacle.append(Obstacle(PIQUE, [1000, 250], [1050, 300], "p"))
+    liste_obstacle.append(Obstacle(PIQUE_REVERSE, [1000, 100], [1050, 150], "p"))
+    liste_obstacle.append(Obstacle(BLOC, [1000, 50], [1050, 100], "b"))
+    liste_obstacle.append(Obstacle(BLOC, [1000, 0], [1050, 50], "b"))
+    liste_obstacle.append(Obstacle(BLOC, [1050, 250], [1100, 300], "bs"))
+    liste_obstacle.append(Obstacle(PIQUE, [1100, 250], [1150, 300], "p"))
+    liste_obstacle.append(Obstacle(BLOC, [1320, 200], [1370, 250], "bs"))
+    liste_obstacle.append(Obstacle(BLOC, [1500, 200], [1550, 250], "bs"))
+    liste_obstacle.append(Obstacle(BLOC, [1550, 200], [1600, 250], "bs"))
+    liste_obstacle.append(Obstacle(PIQUE_REVERSE, [1600, 100], [1650, 150], "p")) 
+    liste_obstacle.append(Obstacle(BLOC, [1600, 50], [1650, 100], "b"))
+    liste_obstacle.append(Obstacle(BLOC, [1600, 0], [1650, 50], "b"))
+    liste_obstacle.append(Obstacle(PIQUE, [1580, 250], [1630, 300], "p")) 
+    liste_obstacle.append(Obstacle(PIQUE , [1820, 250], [1870, 300], "p"))
+    liste_obstacle.append(Obstacle(PIQUE , [2050, 250], [2100, 300], "p"))
+    liste_obstacle.append(Obstacle(BLOC , [2100, 250], [2150, 300], "bs"))
+    liste_obstacle.append(Obstacle(PIQUE , [2150, 250], [2200, 300], "p"))
+    liste_obstacle.append(Obstacle(PIQUE , [2200, 250], [2250, 300], "p"))
+    liste_obstacle.append(Obstacle(PIQUE , [2250, 250], [2300, 300], "p"))
+    liste_obstacle.append(Obstacle(BLOC , [2300, 250], [2350, 300], "bs"))
+    liste_obstacle.append(Obstacle(GIGACHAD , [2600, 50], [2650, 100], "b"))
+    return liste_obstacle
 
-liste_obstacle = []
-liste_obstacle.append(Obstacle(PIQUE, [1000, 250], [1050, 300], "p"))
-liste_obstacle.append(Obstacle(PIQUE_REVERSE, [1000, 100], [1050, 150], "p"))
-liste_obstacle.append(Obstacle(BLOC, [1000, 50], [1050, 100], "b"))
-liste_obstacle.append(Obstacle(BLOC, [1000, 0], [1050, 50], "b"))
-liste_obstacle.append(Obstacle(BLOC, [1050, 250], [1100, 300], "bs"))
-liste_obstacle.append(Obstacle(PIQUE, [1100, 250], [1150, 300], "p"))
-
-liste_obstacle.append(Obstacle(BLOC, [1320, 200], [1370, 250], "bs"))
-liste_obstacle.append(Obstacle(BLOC, [1500, 200], [1550, 250], "bs"))
-liste_obstacle.append(Obstacle(BLOC, [1550, 200], [1600, 250], "bs"))
-liste_obstacle.append(Obstacle(PIQUE_REVERSE, [1600, 100], [1650, 150], "p")) 
-liste_obstacle.append(Obstacle(BLOC, [1600, 50], [1650, 100], "b"))
-liste_obstacle.append(Obstacle(BLOC, [1600, 0], [1650, 50], "b"))
-"""
-liste_obstacle.append( Obstacle( BLOC , [1000, 0], [1050, 50], "b" ) )
-liste_obstacle.append( Obstacle( PIQUE ,[1280, 250], [1330, 300], "p" ) )
-liste_obstacle.append( Obstacle( BLOC , [1450, 50], [1500, 100], "b" ) )
-liste_obstacle.append( Obstacle( BLOC , [1450, 100], [1500, 150], "b" ) )
-liste_obstacle.append( Obstacle( BLOC , [1450, 150], [1500, 200], "b" ) )
-liste_obstacle.append( Obstacle( BLOC , [1450, 200], [1500, 250], "b" ) )
-liste_obstacle.append( Obstacle( PIQUE , [1620, 250], [1670, 300], "p" ) )
-liste_obstacle.append( Obstacle( PIQUE , [1650, 250], [1700, 300], "p" ) )
-liste_obstacle.append( Obstacle( BLOC , [1750, 250], [1800, 300], "b" ) )
-liste_obstacle.append( Obstacle( BLOC , [2050, 250], [2100, 300], "b" ) )
-"""
-LVL1 = Level(screen, *liste_obstacle)
+def play():
+    """
+    La fonction play permet de créer le niveau et le joueur puis appelle la fonction main()
+    pour lancer le jeu.
+    """
+    Bob = Joueur(LARGEUR//2-25, HAUTEUR_SOL - BOB_HAUTEUR,
+                HAUTEUR_SOL-BOB_HAUTEUR)  # Création du joueur (Bob)
+    liste_obstacle = niveau_obstacle()
+    LVL1 = Level(screen, *liste_obstacle) #Création du niveau (LVL1)
+    main(Bob, LVL1, liste_obstacle)
+    pass
 
 
-def main():
+def main(Bob:Joueur, LVL1:Level, liste_obstacle:List[Obstacle]):
     """
     Fonction principale qui tourne tant que le jeu n'est pas fini
     """
-    Bob = Joueur(LARGEUR//2-25, HAUTEUR_SOL - BOB_HAUTEUR,
-                 HAUTEUR_SOL-BOB_HAUTEUR)  # Création du joueur (Bob)
     Bob_position = (Bob.BOB_X, Bob.BOB_Y)
-    running = True
-    SAUT = False
+    running, Saut = True, False
     screen.blit(FOND, (0, 0))
     screen.blit(BOB, (Bob.BOB_X, Bob.BOB_Y))
     frame = 0
@@ -124,7 +132,7 @@ def main():
         Bob_position = (Bob.BOB_X, Bob.BOB_Y)
 
         # GRAVITE
-        if not SAUT:
+        if not Saut:
             Bob.gravite()
             Bob_position = (Bob.BOB_X, Bob.BOB_Y)
             screen.blit(BOB, Bob_position)
@@ -132,12 +140,16 @@ def main():
         # DEFILEMENT
         LVL1.defilement_obstacle()
         LVL1.defilement_sol()
+
         # VERIFICATION HITBOX
         running = not Bob.isDead(liste_obstacle)
+        if running is False:
+            play()
+            break
 
         # SAUT
-        if SAUT:
-            SAUT, frame = Bob.saut(frame)
+        if Saut:
+            Saut, frame = Bob.saut(frame)
             pygame.event.clear()
         Bob.actualiser_surface(liste_obstacle)
 
@@ -146,12 +158,12 @@ def main():
             if event.type == pygame.QUIT:  # type: ignore
                 running = False
             elif event.type == pygame.KEYDOWN:  # type: ignore
-                SAUT, frame, Bob.V = True, 0, 0
+                Saut, frame, Bob.V = True, 0, 0
 
         # Affichage de la distance parcouru
         pygame.display.update()
-        clock.tick(60)
+        clock.tick(160)
     pygame.quit()  # type: ignore
 
 
-main()
+play()
